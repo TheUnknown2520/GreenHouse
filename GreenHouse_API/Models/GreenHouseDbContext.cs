17270adb846db407db9b1e90e80b3be781ed6e6c
ebru0152 <74714355+ebru0152@users.noreply.github.com>
2021-11-24 12:34:48 +0100

    Oprettet databasen

---
+++ GreenHouse_API/Models/GreenHouseDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GreenHouse_API.Models
+{
+    public class GreenHouseDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder option)
+        {
+            option.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=GreenHouseDB; Integrated Security=True; Connect Timeout=30; Encrypt=False");
+        }
+
+
+    }
+}
