80b00f00b7e84966361b259ec366c1664a941b7e
ebru0152 <74714355+ebru0152@users.noreply.github.com>
2021-12-08 10:37:32 +0100

    Metodetest

--- GreenHouseTest/UnitTest1.cs
+++ GreenHouseTest/UnitTest1.cs
@@ -3,6 +3,7 @@ using GreenHouse_API.Managers;
 using ModelLib;
 using System.Linq;
 using System;
+using System.Collections.Generic;
 
 namespace GreenHouseTest
 {
@@ -52,19 +53,43 @@ namespace GreenHouseTest
         [TestMethod]
         [DataRow(20, 96, 2911  )]
         [DataRow(21, 100, 3011)]
-        public void CreateTest(int temperature, int humidity, DateTime date)
+        public void CreateTest()
         {
             var originalListLength = _mgr.GetAll().Count();
             var CreateClima = new Klima( date, temperature, humidity);
 
             var result = _mgr.Create(CreateClima);
-            Assert.IsTrue(result);
-
+            Assert.AreEqual(96, result.Temperature);
+            
             //tjekker at list størelsen er blevet støre 
             var newListLength = _mgr.GetAll().Count();
             Assert.AreEqual(originalListLength + 1, newListLength);
         }
 
+        [TestMethod]
+        public void GetLast24HoursList()
+        {
+            List<Klima> døgList = new List<Klima>();
+            døgList = _mgr.GetLast24Hours();
+            Assert.AreEqual(2, døgList.Count);
 
+            Klima klima1 = new Klima(DateTime.Now.AddHours(-23), 24, 2088);
+            _mgr.GetAll().Add(klima1);
+            døgList = _mgr.GetLast24Hours();
+            Assert.AreEqual(3, døgList.Count, 0.001);
+            Cleanup();
+
+            Klima klima2 = new Klima(DateTime.Now.AddHours(-24), 24, 2088);
+            _mgr.GetAll().Add(klima2);
+            døgList = _mgr.GetLast24Hours();
+            Assert.AreEqual(2, døgList.Count, 0.001);
+            Cleanup();
+
+            Klima klima3 = new Klima(DateTime.Now.AddHours(-25), 24, 2088);
+            _mgr.GetAll().Add(klima3);
+            døgList = _mgr.GetLast24Hours();
+            Assert.AreEqual(2, døgList.Count, 0.001);
+
+        }
     }
 }
