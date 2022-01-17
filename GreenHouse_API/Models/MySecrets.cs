804923c0cc5dd736bc886bdc37be7e0406b57ada
Usama Ahmad <usam0034@edu.easj.dk>
2022-01-17 13:53:24 +0100

    run

--- GreenHouse_API/Models/MySecrets.cs
+++ GreenHouse_API/Models/MySecrets.cs
@@ -7,8 +7,8 @@ namespace GreenHouse_API.Models
 {
     public class MySecrets
     {
-        public const string ConnectionString =
-            @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=GreenDB; Integrated Security=True; Connect Timeout=30; Encrypt=False";
+        //public const string ConnectionString =
+        //    @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=GreenDB; Integrated Security=True; Connect Timeout=30; Encrypt=False";
 
 
     }
